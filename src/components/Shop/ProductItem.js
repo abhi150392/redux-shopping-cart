825c637f50ed0220c69c01a4dd6a8f97d3bc83c2
Abhishek Vijayakumar <abhishekwhizzer@gmail.com>
2023-06-24 17:24:25 -0400

    added redux logic for add and remove cart

--- src/components/Shop/ProductItem.js
+++ src/components/Shop/ProductItem.js
@@ -1,19 +1,32 @@
-import Card from '../UI/Card';
-import classes from './ProductItem.module.css';
+import { useDispatch } from "react-redux";
+import Card from "../UI/Card";
+import classes from "./ProductItem.module.css";
+import { cartSliceActions } from "../../store/cart-slice";
 
 const ProductItem = (props) => {
-  const { title, price, description } = props;
+  const { title, price, description, id } = props;
+  const dispatch = useDispatch();
+
+  const addToCArtHandler = () => {
+    dispatch(
+      cartSliceActions.addItemToCart({
+        id,
+        title,
+        price,
+      })
+    );
+  };
 
   return (
     <li className={classes.item}>
       <Card>
         <header>
-          <h3>{title}</h3>
+          <h3>{props.title}</h3>
           <div className={classes.price}>${price.toFixed(2)}</div>
         </header>
-        <p>{description}</p>
+        <p>{props.description}</p>
         <div className={classes.actions}>
-          <button>Add to Cart</button>
+          <button onClick={addToCArtHandler}>Add to Cart</button>
         </div>
       </Card>
     </li>
